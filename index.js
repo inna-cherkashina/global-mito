2d0199145af3e4a10edf527241b5734eb5d1b462
inna-cherkashina <166111253+inna-cherkashina@users.noreply.github.com>
2025-10-01 13:42:13 +0700

    feat: начата работа над мобильным меню

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.getElementById('burgerMenu');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const closeMenu = document.getElementById('mobile-burger-menu');
+  const mobileOverlay = document.getElementById('mobileOverlay');
+  const mobileDropdowns = document.querySelectorAll('.mobile-dropdown');
 
+  function openMobileMenu() {
+    mobileMenu.classList.add('active');
+    burgerMenu.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function closeMobileMenu() {
+    mobileMenu.classList.remove('active');
+    burgerMenu.classList.remove('active');
+    document.body.style.overflow = '';
+  }
+
+  burgerMenu.addEventListener('click', function () {
+    if (mobileMenu.classList.contains('active')) {
+      closeMobileMenu();
+    } else {
+      openMobileMenu();
+    }
+  });
+
+  closeMenu.addEventListener('click', closeMobileMenu);
+  mobileOverlay.addEventListener('click', closeMobileMenu);
+
+  mobileDropdowns.forEach((dropdown) => {
+    const btnDropdown = dropdown.querySelector('.mobile-dropdown-toggle');
+    btnDropdown.addEventListener('click', function (e) {
+      e.preventDefault();
+      dropdown.classList.toggle('active');
+    });
+  });
+});
